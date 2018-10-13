bcc99f5ba32ad221886f44b7743df3f0b83647c0
gitakang <815101207@qq.com>
2018-10-13 18:47:02 +0800

    akang first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  div{
+    width: 100px;
+    height: 200px;
+    background: #09f;
+  }
+}
+</style>
